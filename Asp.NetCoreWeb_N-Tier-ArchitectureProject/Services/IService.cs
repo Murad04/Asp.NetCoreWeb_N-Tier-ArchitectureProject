d952010ff119e1e071a31292880392808d439389
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-27 14:29:26 +0400

    Added service

---
+++ Asp.NetCoreWeb_N-Tier-ArchitectureProject/Services/IService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreWeb_N_Tier_ArchitectureProject.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIDAsync(int id);
+
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        IQueryable<T> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task RemoveRange(IEnumerable<T> entities);
+    }
+}
