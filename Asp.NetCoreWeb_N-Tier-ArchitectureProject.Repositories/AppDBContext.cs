9afd8f27ece6cf9fdf74e5c2f6d7159217489d98
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-27 18:43:58 +0400

    Added AppDbContext

---
+++ Asp.NetCoreWeb_N-Tier-ArchitectureProject.Repositories/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreWeb_N_Tier_ArchitectureProject.Repositories
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            var feature = new Product() { ProductFeature = new ProductFeature() };
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Product> Product { get; set; } = null!;
+    }
+}
