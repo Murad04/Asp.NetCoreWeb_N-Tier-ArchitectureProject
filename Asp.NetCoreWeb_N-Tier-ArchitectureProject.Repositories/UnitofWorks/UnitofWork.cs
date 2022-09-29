5bacf11fb4be81b7de02333632de3f94d1a71d21
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-29 15:25:17 +0400

    Added UnitofWork class to Repository

---
+++ Asp.NetCoreWeb_N-Tier-ArchitectureProject.Repositories/UnitofWorks/UnitofWork.cs
@@ -0,0 +1,29 @@
+﻿using Asp.NetCoreWeb_N_Tier_ArchitectureProject.UnitofWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreWeb_N_Tier_ArchitectureProject.Repositories.UnitofWorks
+{
+    public class UnitofWork : IUnitofWork
+    {
+        private readonly AppDBContext _dBContext;
+
+        public UnitofWork(AppDBContext dBContext)
+        {
+            _dBContext = dBContext;
+        }
+
+        public void Commit()
+        {
+            _dBContext.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _dBContext.SaveChangesAsync();
+        }
+    }
+}
