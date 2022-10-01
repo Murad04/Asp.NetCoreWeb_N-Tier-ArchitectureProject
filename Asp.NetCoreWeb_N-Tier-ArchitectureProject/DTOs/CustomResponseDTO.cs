a2b46915d9d4c588151fc251c6c1222d9c8b576b
Murad <67361462+Murad04@users.noreply.github.com>
2022-10-01 17:44:13 +0400

    Added CustomResponseDTO to Core Layer

---
+++ Asp.NetCoreWeb_N-Tier-ArchitectureProject/DTOs/CustomResponseDTO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreWeb_N_Tier_ArchitectureProject.DTOs
+{
+    public class CustomResponseDTO<T>
+    {
+        public T? Data { get; set; } 
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<String>? Errors { get; set; }
+
+        public static CustomResponseDTO<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDTO<T> { Data = data, StatusCode = statusCode, Errors = null };
+        }
+
+        public static CustomResponseDTO<T> Success(int statusCode)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode, Errors = null };
+        }
+
+        public static CustomResponseDTO<T> Fail(List<string> errors, int statusCode)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode, Errors = errors };
+        }
+
+        public static CustomResponseDTO<T> Fail(string errors, int statusCode)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode, Errors = new List<string> { errors } };
+        }
+    }
+}
