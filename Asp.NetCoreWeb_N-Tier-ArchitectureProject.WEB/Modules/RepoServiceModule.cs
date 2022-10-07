34b4a162a68c5b72d7661486902e0fa9a288405d
Murad <67361462+Murad04@users.noreply.github.com>
2022-10-08 00:56:58 +0400

    Added Index page for Products

---
+++ Asp.NetCoreWeb_N-Tier-ArchitectureProject.WEB/Modules/RepoServiceModule.cs
@@ -0,0 +1,36 @@
+﻿using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Repositories;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Repositories.Repositories;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Repositories.UnitofWorks;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Service.Mapping;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Service.Services;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Services;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.UnitofWorks;
+using Autofac;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace Asp.NetCoreWeb_N_Tier_ArchitectureProject.WEB.Modules
+{
+    public class RepoServiceModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(GenericRepository<>)).As(typeof(IGenericRepository<>)).InstancePerLifetimeScope();
+            builder.RegisterGeneric(typeof(Service<>)).As(typeof(IService<>)).InstancePerLifetimeScope();
+
+            builder.RegisterType<UnitofWork>().As<IUnitofWork>();
+
+            var apiAssembly = Assembly.GetExecutingAssembly();
+            var repoAssembly = Assembly.GetAssembly(typeof(AppDBContext));
+            var serviceAssembly = Assembly.GetAssembly(typeof(MapProfile));
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                .Where(x => x.Name.EndsWith("Repository")).AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(apiAssembly, repoAssembly, serviceAssembly)
+                    .Where(x => x.Name.EndsWith("Service")).AsImplementedInterfaces()
+                    .InstancePerLifetimeScope();
+        }
+    }
+}
--- Asp.NetCoreWeb_N-Tier-ArchitectureProject.WEB/Program.cs
+++ Asp.NetCoreWeb_N-Tier-ArchitectureProject.WEB/Program.cs
@@ -1,7 +1,32 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.WEB.Modules;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Service.Mapping;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddAutoMapper(typeof(MapProfile));
+builder.Services.AddDbContext<AppDBContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), options =>
+    {
+        options.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDBContext))?.GetName().Name);
+    });
+});
+
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    containerBuilder.RegisterModule(new RepoServiceModule());
+});
 
 var app = builder.Build();
 
