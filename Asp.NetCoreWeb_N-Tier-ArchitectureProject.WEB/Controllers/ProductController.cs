e4deb08a0a7fe7f36addda7ff5ae83443705dcea
Murad <67361462+Murad04@users.noreply.github.com>
2022-10-07 19:05:11 +0400

    Added ProductController and View to WEB Layer

---
+++ Asp.NetCoreWeb_N-Tier-ArchitectureProject.WEB/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Asp.NetCoreWeb_N_Tier_ArchitectureProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCoreWeb_N_Tier_ArchitectureProject.WEB.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customResponse = await _productService.GetProductsWithCategory();
+            return View(customResponse.Data);
+        }
+    }
+}
