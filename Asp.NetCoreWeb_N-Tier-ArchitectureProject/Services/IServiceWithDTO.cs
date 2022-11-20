f9db1f3e00cd220bf672465b1b6731c3ab763641
Murad <67361462+Murad04@users.noreply.github.com>
2022-11-20 20:49:50 +0400

    Added IServiceWithDTO

---
+++ Asp.NetCoreWeb_N-Tier-ArchitectureProject/Services/IServiceWithDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreWeb_N_Tier_ArchitectureProject.Services
+{
+    internal interface IServiceWithDTO
+    {
+    }
+}
